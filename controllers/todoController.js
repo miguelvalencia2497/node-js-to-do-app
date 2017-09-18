bda4f7a82b88060fd576e4b44a4660a0d33b9e78
miguelvalencia <miguelvalencia2497@gmail.com>
2017-09-18 22:59:10 +0800

    Updated app post, get and delete connected to MonggoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -13,34 +13,32 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo', todoSchema);
 
-var itemOne = Todo({
-	item: 'watch movie'
-}).save();
-
-var data = [
-		{item: 'get milk'},
-		{item: 'walk dog'},
-		{item: 'kick some coding ass'}
-	];
-
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
 
     app.get('/todo', function(req, res) {
-    	res.render('todo', {todos: data});
+    	//get data from mongodb and pass it to view
+    	Todo.find({}, function(err, data){
+    		if (err) throw err;
+    		res.render('todo', {todos: data});
+    	});
     });
 
     app.post('/todo', urlencodedParser, function(req, res) {
-    	data.push(req.body);
-    	res.json(data);
+    	//get data from view and add it to monggo db
+    	var newTodo = Todo(req.body).save(function(err, data){
+    		if (err) throw err;
+    		res.json(data);
+    	});
     });
 
     app.delete('/todo/:item', function(req, res) {
-    	data = data.filter(function(todo){
-    		return todo.item.replace(/ /g, '-') !== req.params.item;
+    	//delete item from monggodb
+    	Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+    		if (err) throw err;
+			res.json(data);
     	});
-    	res.json(data);
     });
 
 };
